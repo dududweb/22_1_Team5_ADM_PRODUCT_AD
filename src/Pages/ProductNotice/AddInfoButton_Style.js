9a687841e5c944af35706d4bca5fa61357386218
JooyoungHwnag <joodudsdx@gmail.com>
2022-01-28 22:00:00 +0900

    Add: 상품정보공시 레이아웃 완료

---
+++ src/Pages/ProductNotice/AddInfoButton_Style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const AddInfoButton = styled.div`
+  padding: 25px 5px;
+  width: 100%;
+  text-align: center;
+  font-size: 20px;
+  color: black;
+  border: 1px solid #000;
+  border-radius: 5px;
+  margin: 30px 0;
+`;
